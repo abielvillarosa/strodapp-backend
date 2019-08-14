46549315e160486bb75f3fd05b962b13bed2ae4c
Abiel Villarosa <john_abiel@yahoo.com>
2019-08-14 18:23:26 -0400

    Added firebase function endpoints

--- functions/db/resto.js
+++ functions/db/resto.js
@@ -2,7 +2,7 @@ exports.getStro = async (
     database
 ) => {
     const response = await database.ref().once('value');
-    console.log('response', response.val())
+    // console.log('response', response.val())
     return response.val();
 }
 
@@ -15,6 +15,34 @@ exports.updateViews = async (
     const newViews = currentViews + 1
     updates[path] = newViews
     return database.ref().update(updates)
+}
+
+exports.incrementRestoCounter = async (
+    database,
+    currentCounter
+) => {
+    const updates = {}
+    const path = 'restouid'
+    const newCounter = currentCounter + 1
+    updates[path] = newCounter
+    // return database.ref().update(updates)
+    const response = await database.ref().update(updates)
+    // console.log('responsed', response.val())
+    return database.ref().update(updates)
+}
+
+exports.addNewResto = async (
+    database,
+    restoUid,
+    restoName,
+    restoAddress
+) => {
+    const response = await database.ref(`restaurants/${restoUid}`)
+    response.set({
+        'restoName': restoName, 
+        'restoAddress': restoAddress
+    })
+    return { msg : 'success'}
 }
 
 // exports.updateUserId = async (
--- functions/index.js
+++ functions/index.js
@@ -25,9 +25,41 @@ exports.getStro = functions.https.onRequest((req, res) => {
             // let UserId = 'abvillarosa'
             // let Password = 'Pass123'
             // const password = await website.updateUserId(admin.database(), UserId, Password)
-            console.log(Stro)
+            // console.log(Stro)
             console.log('Views: ' + Views)
             return res.status(200).json(Stro)
+        } catch (err){
+            return res.status(500).json(err)
+        }
+    })
+})
+
+exports.getRestoUid = functions.https.onRequest((req, res) => {
+    return cors(req, res, async () => {
+        try {
+            const Stro = await website.getStro(admin.database())
+            const restoUid = Stro.restouid
+            const newrestoUid = await website.incrementRestoCounter(admin.database(), restoUid)
+            console.log('newrestoUid: ' + newrestoUid)
+            const newStro = await website.getStro(admin.database())
+            return res.status(200).json(newStro)
+        } catch (err){
+            return res.status(500).json(err)
+        }
+    })
+})
+
+exports.addNewResto = functions.https.onRequest((req, res) => {
+    return cors(req, res, async () => {
+        try {
+            var restoUid = 6
+            var restoName = 'bk'
+            var restoAddress = '0x123456'
+            console.log('req', req)
+            console.log('reqtext', req.body)
+            const newResto = await website.addNewResto(admin.database(), restoUid, restoName, restoAddress)
+            const newStro = await website.getStro(admin.database())
+            return res.status(200).json(newStro)
         } catch (err){
             return res.status(500).json(err)
         }
